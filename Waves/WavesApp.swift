9f911f0db875b9d259de7aaf62d30da1eb2e293f
CodingOnBush <mostafa.omrane@proton.me>
2024-10-03 09:56:57 +0200

    Initial Commit

---
+++ Waves/WavesApp.swift
@@ -0,0 +1,32 @@
+//
+//  WavesApp.swift
+//  Waves
+//
+//  Created by Mostafa on 03/10/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WavesApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
